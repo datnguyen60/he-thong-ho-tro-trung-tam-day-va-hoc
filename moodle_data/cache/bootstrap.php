3ff4797d527b45bbcd6cf8a6bbba454130636280
dave <duc.dat@beehexa.com>
2025-03-04 10:18:24 +0700

    Remove moodle_data from tracking

--- moodle_data/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = '9EVy0lzwhv98nYBQHVO3RUNcHOsCbGHMlocalhost';
-$CFG->bootstraphash = '5d246358791a4e200eaedb6dc008a359';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
