516c96c4aaf39d2013bb08ead22cb266831ed9c3
Vo Phuong <maiphuongbrt9a1@gmail.com>
2025-06-06 18:12:01 +0700

    children_management function ver1.0

--- moodle/config.php
+++ moodle/config.php
@@ -35,3 +35,12 @@ require_once(__DIR__ . '/lib/setup.php');
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
+@ini_set('display_errors', '1'); // NOT FOR PRODUCTION SERVERS!
+$CFG->debug = 32767;         // DEBUG_DEVELOPER // NOT FOR PRODUCTION SERVERS!
+// for Moodle 2.0 - 2.2, use:  $CFG->debug = 38911;  
+$CFG->debugdisplay = true;   // NOT FOR PRODUCTION SERVERS!
+
+// You can specify a comma separated list of user ids that that always see
+// debug messages, this overrides the debug flag in $CFG->debug and $CFG->debugdisplay
+// for these users only.
+$CFG->debugusers = '2';
---
+++ moodle/local/children_management/lang/en/local_children_management.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language pack for local_children_management
+ *
+ * @package    local_children_management
+ * @category   string
+ * @copyright  2025 Võ Mai Phương <vomaiphuonghhvt@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// en/local_parentportal.php
+$string['pluginname'] = 'local_children_management';
+
+$string['children_management_title'] = 'My Children';
+$string['children_management_heading'] = 'List of My Children';
+$string['children_list'] = 'Your Children';
+$string['nochildrenfound'] = 'No children linked to your account.';
+$string['viewprofile'] = 'View Profile';
+
+// // vi/local_parentportal.php
+// $string['pluginname'] = 'Cổng Phụ huynh';
+// $string['parentportaltitle'] = 'Con của tôi';
+// $string['parentportalheading'] = 'Danh sách các con của tôi';
+// $string['yourchildren'] = 'Các con của bạn';
+// $string['nochildrenfound'] = 'Không tìm thấy học sinh nào liên kết với tài khoản của bạn.';
+// $string['viewprofile'] = 'Xem Hồ sơ';
