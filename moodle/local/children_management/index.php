4d1cbc0d2f92482093b89e2cd3f63aa86a90c4aa
Vo Phuong <maiphuongbrt9a1@gmail.com>
2025-06-10 22:57:48 +0700

    install moodle checker plugin

--- moodle/local/children_management/index.php
+++ moodle/local/children_management/index.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * TODO describe file index
  *
@@ -56,7 +71,6 @@ try {
                 $actions,
             ];
         }
-        
         echo html_writer::table($table);
         echo html_writer::end_tag('div');
     }
@@ -135,7 +149,7 @@ try {
 
     // Add a button to view child resources.
     $viewresources = new moodle_url('/local/children_management/view_resources.php');
-    echo $OUTPUT->single_button($viewresources, get_string('view_child_resources', 'local_children_management'), 'get', ['class' => 'btn btn-info mt-3']);  
+    echo $OUTPUT->single_button($viewresources, get_string('view_child_resources', 'local_children_management'), 'get', ['class' => 'btn btn-info mt-3']);
 
     echo $OUTPUT->footer();
 } catch (Exception $e) {
